8cb4382fa69ec591357344b340f6ae88a5beb954
wxdao <waxiadao@gamil.com>
2016-09-17 13:05:51 +0800

    cache config

--- src/main/kotlin/com/vecsight/oj/config/ConfigHelper.kt
+++ src/main/kotlin/com/vecsight/oj/config/ConfigHelper.kt
@@ -3,11 +3,18 @@ package com.vecsight.oj.config
 import com.vecsight.oj.Application
 import org.slf4j.LoggerFactory
 import java.net.URI
+import java.util.*
 
 object ConfigHelper {
+    val cache = HashMap<String, Config>()
     fun <T : Config> retrieve(clazz: Class<T>, uri: URI = URI.create("file:///etc/oj/${clazz.canonicalName}.json"), returnNullWhenFailed: Boolean = false): T? {
         val logger = LoggerFactory.getLogger("ConfigHelper")
         val mapper = Application.context!!.getObjectMapper()
+        val cached = cache.get(clazz.canonicalName)
+        if (cached != null) {
+            @Suppress("UNCHECKED_CAST")
+            return cached as T
+        }
         var res: T? = null
         try {
             val stream = uri.toURL().openStream()
