abcce0fb232022858b0df72805d72653029f7e09
wxdao <waxiadao@gamil.com>
2016-09-18 22:56:26 +0800

    name binding fix

--- src/main/kotlin/com/vecsight/oj/resource/annotations.kt
+++ src/main/kotlin/com/vecsight/oj/resource/annotations.kt
@@ -1,5 +1,11 @@
 package com.vecsight.oj.resource
 
+import javax.ws.rs.NameBinding
+
+@NameBinding
+@Retention(AnnotationRetention.RUNTIME)
 annotation class IpLimit
 
+@NameBinding
+@Retention(AnnotationRetention.RUNTIME)
 annotation class AuthRequired
